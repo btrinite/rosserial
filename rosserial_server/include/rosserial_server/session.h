07c2a77ec1cff899fd467a063cc02db48d88320e
Benoit TRINITE <btrinite@free.fr>
2020-05-25 14:40:32 +0200

    increase buffer (for video packet)

--- rosserial_server/include/rosserial_server/session.h
+++ rosserial_server/include/rosserial_server/session.h
@@ -503,7 +503,7 @@ private:
   boost::asio::io_service& io_service_;
   Socket socket_;
   AsyncReadBuffer<Socket> async_read_buffer_;
-  enum { buffer_max = 1023 };
+  enum { buffer_max = 64000 };
   bool active_;
 
   ros::NodeHandle nh_;
